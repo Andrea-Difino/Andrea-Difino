0bdb9e2e8476c04e0c8b728c6829d73c8cd8f177
Andrea-Difino <adifino13@gmail.com>
2023-02-07 21:03:35 +0100

    insertion sort modified

--- Works/JAVA/algorithms/insertionSort/src/Main.java
+++ Works/JAVA/algorithms/insertionSort/src/Main.java
@@ -4,30 +4,24 @@ public class Main {
     public static void main(String[] args) {
 
         int[] arr = {5, 4, 2, 6, 3};
-        int[] new_arr = sort(arr);
-        System.out.println("Sorted Array" + "\n" + Arrays.toString(new_arr));
+        int n = arr.length;
+        sort(arr, n);
+        System.out.println("Sorted Array" + "\n" + Arrays.toString(arr));
     }
 
-    public static int[] sort(int[] a){
+    public static int[] sort(int[] a, int n){
 
-        int[] new_arr = new int[a.length + 1];
-        new_arr[0] = 0;
-        for (int j = 0; j < a.length; j++) {
-            new_arr[j+1] = a[j];
-        }
-        //{0, 5, 4, 2, 6, 3}
-        int n = new_arr.length;
-
-        for (int i = 2; i < n; i++) {
-            int key = new_arr[i];
+        for (int i = 1; i < n+1; i++) {
+            int key = a[i-1];
             int j = i - 1;
-            while(new_arr[j] > key && j > 0){
-                new_arr[j+1] = new_arr[j];
+            while(j > 0 && a[j-1] > key ){
+                a[j] = a[j-1];
                 j = j - 1;
             }
-            new_arr[j+1] = key;
+            a[j] = key;
+            System.out.println(Arrays.toString(a));
         }
 
-        return new_arr;
+        return a;
     }
 }
