78b4d1047c7f42964ffd7bdd144970bf4d902c86
Andrea-Difino <adifino13@gmail.com>
2023-08-25 14:32:27 +0200

    add LostInSpace game

---
+++ Works/JAVA/LostInSpace/src/Main.java
@@ -0,0 +1,10 @@
+import javax.sound.sampled.*;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws UnsupportedAudioFileException, LineUnavailableException, IOException {
+
+        new MyFrame();
+
+    }
+}
